06b86195f6c9d36ceb3a92ae13fe333f9ff87edc
LDY <137361731+ldsldy@users.noreply.github.com>
2025-09-29 17:54:13 +0900

    코드 옮기는 중

--- Kyungil_WindowAPI_Shooting2d.cpp
+++ Kyungil_WindowAPI_Shooting2d.cpp
@@ -15,7 +15,13 @@ HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
-
+Gdiplus::Point g_HousePosition(100, 100);
+constexpr int g_HouseVertexCount = 7;
+const Gdiplus::Point g_HouseVertex[g_HouseVertexCount] =
+{
+    {0,-100}, {50,-50},{30,-50},{30,0},{-30,0},{-30,-50},{-50,-50}
+};
+std::unordered_map<InputDirection, bool> g_KeyWasPressedMap;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
